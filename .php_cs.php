cdff905d76812571d60bff8d835ca94de924514f
Ilija Studen <ilija.studen@activecollab.com>
2016-03-16 11:26:24 +0100

    Fix header comment

--- .php_cs.php
+++ .php_cs.php
@@ -7,7 +7,7 @@
  */
 
 $header = <<<EOF
-This file is part of the Active Collab ID project.
+This file is part of the Active Collab Authentication project.
 
 (c) A51 doo <info@activecollab.com>. All rights reserved.
 EOF;
