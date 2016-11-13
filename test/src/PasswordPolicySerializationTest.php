98872483b3fa5a65a2881c569d5d2bc8fdeae5e6
Ilija Studen <ilija.studen@activecollab.com>
2016-11-13 23:52:08 +0100

    Test login and password policy JSON serialization

---
+++ test/src/PasswordPolicySerializationTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Authentication project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+namespace ActiveCollab\Authentication\Test;
+
+use ActiveCollab\Authentication\Password\Policy\PasswordPolicy;
+use ActiveCollab\Authentication\Test\TestCase\TestCase;
+use JsonSerializable;
+
+/**
+ * @package ActiveCollab\Authentication\Test
+ */
+class PasswordPolicySerializationTest extends TestCase
+{
+    public function testPasswordPolicyCanBeSerialized()
+    {
+        $this->assertInstanceOf(JsonSerializable::class, new PasswordPolicy());
+    }
+
+    public function testJsonSerialization()
+    {
+        $default_policy = new PasswordPolicy();
+
+        $data = json_decode(json_encode($default_policy), true);
+        $this->assertInternalType('array', $data);
+
+        $this->assertArrayHasKey('min_length', $data);
+        $this->assertArrayHasKey('require_numbers', $data);
+        $this->assertArrayHasKey('require_mixed_case', $data);
+        $this->assertArrayHasKey('require_symbols', $data);
+
+        $this->assertSame(0, $data['min_length']);
+        $this->assertSame(false, $data['require_numbers']);
+        $this->assertSame(false, $data['require_mixed_case']);
+        $this->assertSame(false, $data['require_symbols']);
+
+        $customized_policy = new PasswordPolicy(32, true, true, true);
+
+        $data = json_decode(json_encode($customized_policy), true);
+
+        $this->assertArrayHasKey('min_length', $data);
+        $this->assertArrayHasKey('require_numbers', $data);
+        $this->assertArrayHasKey('require_mixed_case', $data);
+        $this->assertArrayHasKey('require_symbols', $data);
+
+        $this->assertSame(32, $data['min_length']);
+        $this->assertSame(true, $data['require_numbers']);
+        $this->assertSame(true, $data['require_mixed_case']);
+        $this->assertSame(true, $data['require_symbols']);
+    }
+}
