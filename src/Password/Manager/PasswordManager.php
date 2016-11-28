cc2ef84f79a436c6ba96b26ba7d1205ecc38feb6
Pera Jovic <perajovic@me.com>
2016-11-28 09:05:07 +0100

    Run CS fixer

--- src/Password/Manager/PasswordManager.php
+++ src/Password/Manager/PasswordManager.php
@@ -87,27 +87,27 @@ class PasswordManager implements PasswordManagerInterface
      */
     private function pbkdf2($p, $s, $c, $kl, $a = 'sha256')
     {
-        $hl = strlen(hash($a, null, true)); # Hash length
-        $kb = ceil($kl / $hl);              # Key blocks to compute
-        $dk = '';                           # Derived key
+        $hl = strlen(hash($a, null, true)); // Hash length
+        $kb = ceil($kl / $hl);              // Key blocks to compute
+        $dk = '';                           // Derived key
 
-        # Create key
+        // Create key
         for ($block = 1; $block <= $kb; ++$block) {
 
-            # Initial hash for this block
+            // Initial hash for this block
             $ib = $b = hash_hmac($a, $s . pack('N', $block), $p, true);
 
-            # Perform block iterations
+            // Perform block iterations
             for ($i = 1; $i < $c; ++$i) {
-                # XOR each iterate
+                // XOR each iterate
 
                 $ib ^= ($b = hash_hmac($a, $b, $p, true));
             }
 
-            $dk .= $ib; # Append iterated block
+            $dk .= $ib; // Append iterated block
         }
 
-        # Return derived key of correct length
+        // Return derived key of correct length
 
         return substr($dk, 0, $kl);
     }
