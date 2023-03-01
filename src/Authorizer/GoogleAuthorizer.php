a94ad627862e23ad7e4978977e0e286575593e21
Rajko Stupar <rajko.stupar@activecollab.com>
2023-03-01 12:05:07 +0100

    Add to use email from payload

--- src/Authorizer/GoogleAuthorizer.php
+++ src/Authorizer/GoogleAuthorizer.php
@@ -21,37 +21,25 @@ use Google_Client;
 class GoogleAuthorizer extends Authorizer
 {
     use CredentialFieldsCheckTrait;
-
-    /**
-     * @var RepositoryInterface
-     */
-    private $user_repository;
-
-    /**
-     * @var Google_Client
-     */
-    private $google_client;
-
-    /**
-     * @var string
-     */
-    private $client_id;
-
-    /**
-     * @var array
-     */
-    private $user_profile;
+    private RepositoryInterface $user_repository;
+    private Google_Client $google_client;
+    private string $client_id;
+    private array $user_profile;
 
     /**
      * GoogleAuthorizer constructor.
      *
-     * @param RepositoryInterface            $user_repository
-     * @param Google_Client                  $google_client
-     * @param                                $client_id
+     * @param RepositoryInterface $user_repository
+     * @param Google_Client $google_client
+     * @param string $client_id
      * @param ExceptionHandlerInterface|null $exception_handler
      */
-    public function __construct(RepositoryInterface $user_repository, Google_Client $google_client, $client_id, ExceptionHandlerInterface $exception_handler = null)
-    {
+    public function __construct(
+        RepositoryInterface $user_repository,
+        Google_Client $google_client,
+        string $client_id,
+        ExceptionHandlerInterface $exception_handler = null
+    ) {
         $this->user_repository = $user_repository;
         $this->google_client = $google_client;
         $this->client_id = $client_id;
@@ -65,16 +53,15 @@ class GoogleAuthorizer extends Authorizer
      *
      * {@inheritdoc}
      */
-    public function verifyCredentials(array $credentials)
+    public function verifyCredentials(array $credentials): ?AuthenticatedUserInterface
     {
-        $this->verifyRequiredFields($credentials, ['token', 'username']);
-
+        $this->verifyRequiredFields($credentials, ['token']);
         $token = $credentials['token'];
-        $username = $credentials['username'];
 
         $payload = $this->google_client->verifyIdToken($token);
+        $username = $payload['email'] ?? $credentials['username'];
 
-        $this->verifyGoogleProfile($payload, $username);
+        $this->verifyGoogleProfile($payload, $credentials['username'] ?? null);
         $this->user_profile = $payload;
 
         $user = $this->user_repository->findByUsername($username);
@@ -86,16 +73,16 @@ class GoogleAuthorizer extends Authorizer
     /**
      * @return array
      */
-    public function getUserProfile()
+    public function getUserProfile(): array
     {
         return $this->user_profile;
     }
 
     /**
-     * @param array  $payload
-     * @param string $username
+     * @param array $payload
+     * @param string|null $username
      */
-    private function verifyGoogleProfile(array $payload, $username)
+    private function verifyGoogleProfile(array $payload, ?string $username): void
     {
         if ($this->client_id !== $payload['aud']) {
             throw new RuntimeException('Unrecognized google_client');
@@ -105,7 +92,7 @@ class GoogleAuthorizer extends Authorizer
             throw new RuntimeException('Wrong issuer');
         }
 
-        if ($username !== $payload['email']) {
+        if ($username && $username !== $payload['email']) {
             throw new RuntimeException('Email is not verified by Google');
         }
     }
@@ -113,7 +100,7 @@ class GoogleAuthorizer extends Authorizer
     /**
      * @param AuthenticatedUserInterface|null $user
      */
-    private function verifyUser(AuthenticatedUserInterface $user = null)
+    private function verifyUser(AuthenticatedUserInterface $user = null): void
     {
         if (!$user || !$user->canAuthenticate()) {
             throw new UserNotFoundException();
@@ -123,7 +110,7 @@ class GoogleAuthorizer extends Authorizer
     /**
      * @return array
      */
-    private function getDomains()
+    private function getDomains(): array
     {
         return ['accounts.google.com', 'https://accounts.google.com'];
     }
